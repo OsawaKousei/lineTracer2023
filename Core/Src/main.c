a41949e094f9471ae9fa6a1ae19c33a9735db14a
OsawaKousei <jwithelis@gmail.com>
2023-05-22 16:28:55 +0900

    working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,7 +121,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
 	while (1) {
 
-		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 5000);
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 60);
 //		HAL_Delay(500);
 //		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
 //		HAL_Delay(500);
@@ -365,9 +365,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 127;
+  htim1.Init.Prescaler = 1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 10000;
+  htim1.Init.Period = 500;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
